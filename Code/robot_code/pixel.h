169b632c3656a3542edeab00b6484ef0aab3a239
jannikgade <jannikgade@gmail.com>
2014-12-02 15:39:10 +0100

    added functionality in pixel class

--- Code/robot_code/pixel.h
+++ Code/robot_code/pixel.h
@@ -24,16 +24,35 @@ public:
     pixel& operator=(const pixel &p);
     operator bool() const;
     
+    friend const pixel operator*(const pixel& v, double s)
+    {
+        return pixel(v.x * s, v.y * s);
+    }
+    
+    pixel& operator*=(double s)
+    {
+        x*=s;
+        y*=s;
+        return *this;
+    }
+    
     void set_pixel_x_y(int x_value, int y_value);
     int get_x(void);
     int get_y();
     bool is_uninitialized(void);
 };
 
+
 bool operator==(pixel &p1, pixel &p2);
 bool operator!=(pixel &p1, pixel &p2);
 
 
+/*
+ friend const Vector2D operator*(const Vector2D& v, double s)
+	{
+ return Vector2D(v[0] * s, v[1] * s);
+	}
+ */
 
 #endif /* defined(__CUP_COLLECTOR__pixel__) */
 
