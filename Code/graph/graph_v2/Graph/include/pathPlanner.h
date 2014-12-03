178a3899f1506758b88a53e4f36fb3da47dbe279
Anders Launer Bæk <anbae12@student.sdu.dk>
2014-12-03 11:17:30 +0100

    Jeg kan få den til at læse koorent fra køen.. Nu er der bare nogle koordinater som driller, puls den vil ikke vaske det hele.. Jeg har lagt min egen projekt mappe på git.. Heri er de rettelser jeg har lavet..

--- Code/graph/graph_v2/Graph/include/pathPlanner.h
+++ Code/graph/graph_v2/Graph/include/pathPlanner.h
@@ -10,47 +10,57 @@
 #include <limits>
 #include <list>
 
+#include "robot.h"
+#include "findPoints.h"
 using namespace std;
 
-class pathPlanner
+class pathPlanner// : public cup_robot
 {
     public:
-        pathPlanner();
-        ~pathPlanner();
+      pathPlanner();
+      //~pathPlanner();
         void addVertex(unsigned int topX, unsigned int topY, unsigned int bottomX, unsigned int bottomY);
         void getPath();
+    
+    typedef std::queue<coordinatesPair(int,int,int,int)> queue1;
+    queue1 toJannik;
+    
+    
+    struct coord {
+        //coord(unsigned int x, unsigned int y) : x(x), y(y){};
+        unsigned int x = 0, y = 0;
+    };
+    
+    struct vertex {
+        list<unsigned int> adj;
+        unsigned int dist;
+        std::vector<unsigned int> pathGrid;
+        unsigned int identifier = 0;
+        
+        coord top, bottom;
+        void setCoord(unsigned int topX, unsigned int topY, unsigned int bottomX, unsigned int bottomY) {
+            top.x = topX;
+            top.y = topY;
+            bottom.x = bottomX;
+            bottom.y = bottomY;
+        }
+        
+        /*vertex() {
+         for (unsigned int i = 0; i < 24; i++)
+         pathGrid[i] = infinityCost;
+         }*/
+    };
+    
+    typedef vector<vertex> vertices_t;
+    vertices_t vertices;
+    
+    list<unsigned int> path;
+    
+    
     private:
         const static unsigned int infinityCost = numeric_limits<unsigned int>::max();
 
-        struct coord {
-            //coord(unsigned int x, unsigned int y) : x(x), y(y){};
-            unsigned int x = 0, y = 0;
-        };
-
-        struct vertex {
-            list<unsigned int> adj;
-            unsigned int dist;
-            unsigned int pathGrid[24];
-            unsigned int identifier = 0;
-
-            coord top, bottom;
-            void setCoord(unsigned int topX, unsigned int topY, unsigned int bottomX, unsigned int bottomY) {
-                top.x = topX;
-                top.y = topY;
-                bottom.x = bottomX;
-                bottom.y = bottomY;
-            }
-
-            vertex() {
-                for (unsigned int i = 0; i < 24; i++)
-                    pathGrid[i] = infinityCost;
-            }
-        };
-
-        typedef vector<vertex> vertices_t;
-        vertices_t vertices;
-
-        list<unsigned int> path;
+    
 
         void createEdges();
         void createPathGrids();
